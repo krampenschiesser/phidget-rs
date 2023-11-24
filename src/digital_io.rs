7aba48e4de48b17efdd4aacb069d5eb305e8022d
krampenschiesser <krampenschiesser@gmail.com>
2023-11-24 09:51:27 -0700

    failsafe for digital out

--- src/digital_io.rs
+++ src/digital_io.rs
@@ -74,8 +74,8 @@ impl DigitalInput {
 
     /// Sets a handler to receive digital input state change callbacks.
     pub fn set_on_state_change_handler<F>(&mut self, cb: F) -> Result<()>
-    where
-        F: Fn(&DigitalInput, i32) + Send + 'static,
+        where
+            F: Fn(&DigitalInput, i32) + Send + 'static,
     {
         // 1st box is fat ptr, 2nd is regular pointer.
         let cb: Box<Box<DigitalInputCallback>> = Box::new(Box::new(cb));
@@ -93,8 +93,8 @@ impl DigitalInput {
 
     /// Sets a handler to receive attach callbacks
     pub fn set_on_attach_handler<F>(&mut self, cb: F) -> Result<()>
-    where
-        F: Fn(&GenericPhidget) + Send + 'static,
+        where
+            F: Fn(&GenericPhidget) + Send + 'static,
     {
         let ctx = crate::phidget::set_on_attach_handler(self, cb)?;
         self.attach_cb = Some(ctx);
@@ -103,8 +103,8 @@ impl DigitalInput {
 
     /// Sets a handler to receive detach callbacks
     pub fn set_on_detach_handler<F>(&mut self, cb: F) -> Result<()>
-    where
-        F: Fn(&GenericPhidget) + Send + 'static,
+        where
+            F: Fn(&GenericPhidget) + Send + 'static,
     {
         let ctx = crate::phidget::set_on_detach_handler(self, cb)?;
         self.detach_cb = Some(ctx);
@@ -203,10 +203,36 @@ impl DigitalOutput {
         ReturnCode::result(unsafe { ffi::PhidgetDigitalOutput_setDutyCycle(self.chan, dc) })
     }
 
+    /// Enables the failsafe feature for the channel, with a given failsafe time.
+    ///
+    /// The failsafe feature is intended for use in applications where it is important for the channel to enter a known safe state if the program controlling it locks up or crashes. If you do not enable the failsafe feature, the channel will carry out whatever instructions it was last given until it is explicitly told to stop.
+    ///
+    /// Enabling the failsafe feature starts a recurring failsafe timer for the channel. Once the failsafe timer is enabled, it must be reset within the specified time or the channel will enter a failsafe state. The failsafe timer may be reset by sending any valid command to the device*. Resetting the failsafe timer will reload the timer with the specified failsafe time, starting when the message to reset the timer is received by the Phidget.
+    ///
+    /// *(get requests do not typically send commands and won't reset the failsafe timer)
+    ///
+    /// For example: if the failsafe is enabled with a failsafe time of 1000ms, you will have 1000ms to reset the failsafe timer. Every time the failsafe timer is reset, you will have 1000ms from that time to reset the failsafe again.
+    ///
+    /// If the failsafe timer is not reset before it runs out, the channel will enter a failsafe state. For Digital Output channels, this will set the output State to FALSE. Once the channel enters the failsafe state, it will reject any further input until the channel is reopened.
+    ///
+    /// To prevent the channel from falsely entering the failsafe state, we recommend resetting the failsafe timer as frequently as is practical for your application. A good rule of thumb is to not let more than a third of the failsafe time pass before resetting the timer.
+    ///
+    /// Once the failsafe timer has been set, it cannot be disabled by any means other than closing and reopening the channel.
+    pub fn enable_failsafe(&self, time: u32) -> Result<()> {
+        ReturnCode::result(unsafe { ffi::PhidgetDigitalOutput_enableFailsafe(self.chan, time) })
+    }
+
+    /// Resets the failsafe timer, if one has been set. See PhidgetDigitalOutput_enableFailsafe() for details.
+    ///
+    ///    This function will fail if no failsafe timer has been set for the channel.
+    pub fn reset_failsafe(&self) -> Result<()> {
+        ReturnCode::result(unsafe { ffi::PhidgetDigitalOutput_resetFailsafe(self.chan) })
+    }
+
     /// Sets a handler to receive attach callbacks
     pub fn set_on_attach_handler<F>(&mut self, cb: F) -> Result<()>
-    where
-        F: Fn(&GenericPhidget) + Send + 'static,
+        where
+            F: Fn(&GenericPhidget) + Send + 'static,
     {
         let ctx = crate::phidget::set_on_attach_handler(self, cb)?;
         self.attach_cb = Some(ctx);
@@ -215,8 +241,8 @@ impl DigitalOutput {
 
     /// Sets a handler to receive detach callbacks
     pub fn set_on_detach_handler<F>(&mut self, cb: F) -> Result<()>
-    where
-        F: Fn(&GenericPhidget) + Send + 'static,
+        where
+            F: Fn(&GenericPhidget) + Send + 'static,
     {
         let ctx = crate::phidget::set_on_detach_handler(self, cb)?;
         self.detach_cb = Some(ctx);
